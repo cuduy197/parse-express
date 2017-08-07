d6c9ebdbfe9aecde92c2895f734910ecf302fad6
cuduy197 <cuduy197@gmail.com>
2017-08-08 05:04:08 +0700

    Update something :zap:

--- pm2.sh
+++ pm2.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 pm2 delete index
 git pull
+if [ $? -eq 0 ]; then
+else
+    echo Reset hard git
+	git reset --hard
+fi
 pm2 start index.js
-pm2 show index
 echo -e "Pm2 hoàn thành! \n"
