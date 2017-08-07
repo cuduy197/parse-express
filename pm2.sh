ba83fb7134c683cbaddfa654a2980b4fcffbad27
cuduy197 <cuduy197@gmail.com>
2017-08-08 05:02:26 +0700

    Update something :zap:

--- pm2.sh
+++ pm2.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
 pm2 delete index
-if [ $? -eq 0 ]; then
-    echo PM2 OK
-else
-    echo USING SUDO
-	sudo pm2 delete index
-fi
-
 git pull
-
 pm2 start index.js
-if [ $? -eq 0 ]; then
-    echo PM2 OK
-else
-    echo USING SUDO
-	sudo pm2 start index.js
-fi
-
+pm2 show index
 echo -e "Pm2 hoàn thành! \n"
